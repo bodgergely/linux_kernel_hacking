b468e19b1a060701572aad606259f5420bb0f335
Gergely Bod <bodgergely@hotmail.com>
2018-07-19 07:53:50 +0100

    new

--- build_linux.sh
+++ build_linux.sh
@@ -1,6 +1,6 @@
-LINUX_SRC=$WORKSPACE/linux
+LINUX_SRC=$HOME/linux/3.7.1/linux-3.7.1/
 BUSYBOX_DIR=$HOME/busybox
 
 cd $LINUX_SRC
 
-make O=$BUSYBOX_DIR/build/linux-x86-basic -j4
+make O=$BUSYBOX_DIR/build/linux-3.7.1 -j4
--- configure_linux.sh
+++ configure_linux.sh
@@ -1,8 +1,10 @@
-LINUX_SRC=$WORKSPACE/linux
+LINUX_SRC=$HOME/linux/3.7.1/linux-3.7.1/
 BUSYBOX_DIR=$HOME/busybox
 
 cd $LINUX_SRC
 
 #make O=$BUSYBOX_DIR/build/linux-x86-basic x86_64_defconfig
-make O=$BUSYBOX_DIR/build/linux-x86-basic i386_defconfig
+make O=$BUSYBOX_DIR/build/linux-3.7.1 i386_defconfig
+#below is to have menu config
+#make O=$BUSYBOX_DIR/build/linux-3.7.1 menuconfig
 #make O=$BUSYBOX_DIR/build/linux-x86-basic kvmconfig

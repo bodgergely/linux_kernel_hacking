2d103304d2def85f008342d54c4251ee64ad0363
Gergely Bod <bodgergely@hotmail.com>
2018-09-14 13:25:56 +0100

    lubuntu boot

--- boot_lubuntu.sh
+++ boot_lubuntu.sh
@@ -1,8 +1,10 @@
 #qemu-system-x86_64 -m 2G -s -hda ~/operating_systems/linux_distros/Lubuntu/lubuntu.img -enable-kvm -kernel ~/busybox/build/linux-x86-basic/arch/x86_64/boot/bzImage -serial stdio -append "root=/dev/sda1 quiet 3"
 
 #from hardrive - normal run - no kvm, 32 bit
-qemu-system-i386 \
-    -m 2G -s -hda ~/operating_systems/linux_distros/Lubuntu/lubuntu_32.img \
-    -kernel ~/busybox/build/linux-x86-basic/arch/i386/boot/bzImage \
-    -nographic -append "root=/dev/sda1 console=ttyS0" 
+qemu-system-x86_64 \
+    -m 2G -s -hda ~/operating_systems/linux/lubuntu/lubuntu_64.img \
+    -kernel ~/busybox/build/linux-master/arch/x86_64/boot/bzImage \
+    -serial stdio \
+    -enable-kvm \
+    -append "root=/dev/sda1 console=ttyS0 nokaslr" 
 

6beaf122ded5ecb7a89f4c2af9a124c11ebc9b70
Gergely Bod <bodgergely@hotmail.com>
2019-10-06 16:14:34 +0100

    add CONFIG_DEBUG_INFO and CONFIG_8139C

--- configure_linux.sh
+++ configure_linux.sh
@@ -10,4 +10,6 @@ make O=$BUSYBOX_DIR/build/linux-$BRANCH x86_64_defconfig
 #below is to have menu config
 #make O=$BUSYBOX_DIR/build/linux-3.7.1 menuconfig
 #make O=$BUSYBOX_DIR/build/linux-x86-basic kvmconfig
+echo "CONFIG_DEBUG_INFO=y" >> $BUSYBOX_DIR/build/linux-$BRANCH/.config
+echo "CONFIG_8139CP=y" >> $BUSYBOX_DIR/build/linux-$BRANCH/.config
 vim $BUSYBOX_DIR/build/linux-$BRANCH/.config
